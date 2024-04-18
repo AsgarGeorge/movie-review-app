09c683f26a749af7ca560265dfb861d909b0e7b4
Asgar_George <asgarantony@gmail.com>
2024-04-18 08:41:05 +0530

    initial commit

---
+++ movies-review-api/src/main/java/com/asgar/moviesreviewapi/MoviesReviewApiApplication.java
@@ -0,0 +1,13 @@
+package com.asgar.moviesreviewapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesReviewApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesReviewApiApplication.class, args);
+	}
+
+}
